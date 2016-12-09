a7327e86199f64fc7581780a5e2d411505767bd1
Ravi Alledulla <ravia@giffgaff.co.uk>
2016-12-09 12:47:50 +0000

    added email.js

---
+++ src/email.js
@@ -0,0 +1,24 @@
+var nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "ravi.alleddulla@gmail.com",
+        pass: "########################"
+    }
+});
+
+smtpTransport.sendMail({
+    from: "giffgaff <ravia@giffgaff.co.uk>", // sender address
+    to: "ravia<ravia@giffgaff.co.uk>", // comma separated list of receivers
+    subject: "Mail from giffgaff phones team", // Subject line
+    text: "Hello world" // attach list of phones to be sent here
+}, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Mail sent: " + response.message);
+    }
+});
+
+});
